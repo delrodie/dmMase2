ee097089415abc79d63d8da8fe912067a90bd246
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-08-23 18:26:48 +0000

    Dev

---
+++ src/Controller/Frontend/FrontendMaintenanceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Service\AllRepositories;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/dev')]
+class FrontendMaintenanceController extends AbstractController
+{
+    public function __construct(
+        private AllRepositories $allRepositories
+    )
+    {
+    }
+
+    #[Route('/', name: 'app_maintenance')]
+    public function index(): Response
+    {
+        return $this->render('frontend/home.html.twig',[
+            'slides' => $this->allRepositories->allCache('slides'),
+            'mission' => $this->allRepositories->allCache('mission'),
+            'actualites' => $this->allRepositories->allCache('actualites'),
+            'entreprises' => $this->allRepositories->allCache('entreprise')
+        ]);
+    }
+}
