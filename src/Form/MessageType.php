0d437fbb9413e07c6071cf7b6c923f3c2c37dce8
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-08-24 11:08:27 +0000

    Backend Message

---
+++ src/Form/MessageType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('email')
+            ->add('objet')
+            ->add('contenu')
+            ->add('lecture')
+            ->add('reponse')
+            ->add('sendAt')
+            ->add('replyAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
